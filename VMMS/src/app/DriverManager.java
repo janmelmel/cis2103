051d285c51d09cd452c43fc501b3503c50c08c97
janmelmel <70429539+janmelmel@users.noreply.github.com>
2022-12-02 17:51:37 +0800

    Add files via upload

---
+++ VMMS/src/app/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Janmel
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306vmms, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
